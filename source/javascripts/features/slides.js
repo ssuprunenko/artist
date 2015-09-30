056bd073bf7694cf17ad4a80e8dff5afdc5ad0fc
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-09-30 23:41:15 +0300

    [fix] Change height of slideshow

--- source/javascripts/features/slides.js
+++ source/javascripts/features/slides.js
@@ -6,12 +6,16 @@
     $slideshow.slick({
       slide: 'li',
       dots: true,
+      infinite: true,
       speed: 400,
+      slidesToShow: 1,
+      centerMode: true,
       fade: false,
       arrows: false,
       draggable: true,
       autoplay: false,
       autoplaySpeed: 3000,
+      variableWidth: true
     });
 
   });
