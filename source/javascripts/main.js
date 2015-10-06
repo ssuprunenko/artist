a5cb3b94298ff08bb880350675cbc705408bb0bd
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-10-06 16:32:37 +0300

    [cleanup] Syntax highlighter

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,7 +1,6 @@
 //= require vendor/lodash.min
 //= require vendor/ismobile.min
 //= require vendor/slick.min
-//= require vendor/prism.min
 //= require vendor/moment.min
 //= require vendor/hogan-3.0.2.min
 //= require_tree ./lib
