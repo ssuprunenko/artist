f6b3f4298b2201d317f9fdb3daab66a04ef845cf
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-11-15 16:55:25 +0300

    [add] Slick-lightbox library

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,6 +1,7 @@
 //= require vendor/lodash.min
 //= require vendor/ismobile.min
 //= require vendor/slick.min
+//= require vendor/slick-lightbox.min
 //= require vendor/moment.min
 //= require vendor/hogan-3.0.2.min
 //= require_tree ./lib
