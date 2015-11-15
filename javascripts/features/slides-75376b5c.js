16e24c5e46636bd24e723c75862821a4b58ac819
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-11-15 17:18:41 +0300

    Automated commit at 2015-11-15 14:18:41 UTC by middleman-deploy 1.0.0

--- javascripts/features/slides-75376b5c.js
+++
@@ -1,1 +0,0 @@
-!function(){window.pageReady(function(){var e=$("[data-slides]");e.slick({slide:"li",dots:!0,infinite:!0,speed:400,slidesToShow:1,centerMode:!0,fade:!1,arrows:!1,draggable:!0,autoplay:!1,autoplaySpeed:3e3,variableWidth:!0})})}();
---
+++ javascripts/features/slides-867659b0.js
@@ -0,0 +1,1 @@
+!function(){window.pageReady(function(){var e=$("[data-slides]");e.slick({slide:"li",dots:!0,infinite:!0,speed:400,slidesToShow:1,centerMode:!0,fade:!1,arrows:!1,draggable:!0,autoplay:!1,autoplaySpeed:3e3,variableWidth:!0}),e.slickLightbox({background:"rgba(0,0,0)",src:"src",itemSelector:"li img"})})}();
